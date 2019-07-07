6d4a642fe8c4587d7025d09f841dd7d016fe9efd
Csányi István <icsanyi96@gmail.com>
2019-07-07 19:58:20 +0200

    Fix fetch has no timeout

--- src/renderer/common.ts
+++ src/renderer/common.ts
@@ -3,7 +3,7 @@ import urljoin from "url-join";
 export const requestUrl = "http://192.168.1.101:11277/";
 
 export async function getJsonFromUrl(url: string): Promise<any> {
-    return fetch(url).then(res => {
+    return fetchTimeout(url, 4000).then(res => {
         if (res.ok) {
             return res.json();
         } else {
@@ -44,4 +44,13 @@ export function getVideoUrl(groupId: string, videoName: string) {
 
 export function getSubUrl(groupId: string, videoName: string) {
     return urljoin(requestUrl, "sub", groupId, encodeURI(videoName));
+}
+
+function fetchTimeout(url: string, timeout: number): Promise<Response> {
+    const req = fetch(url);
+    const tmo = new Promise((_, reject) => {
+        return setTimeout(() => reject(new Error('request timeout')), timeout);
+    });
+
+    return Promise.race([req, tmo]) as Promise<Response>;
 }
